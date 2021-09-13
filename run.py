afaf1942b96a19acb76aa9c145e391db4a65a4a4
bodybreaker <bodybreaker@naver.com>
2021-09-13 22:23:41 +0900

    no message

--- run.py
+++ run.py
@@ -39,40 +39,36 @@ class watchdog:
         self.FileList = list()
         self.checkCount = 0
     def run(self):
-        try:
-            folder = 'C:/Users/pmw18/Downloads/DGB_R&D_Project/dgb_reporter'
-            file = 'YTN_bong_3.wav'
-            split_wav = SplitWavAudioMubin(folder, file)                                 
-            split_wav.multiple_split(min_per_split=1)			## 전체 wav파일을 1분 이하의 파일들로 분리함 
-            self.FileList = sorted(glob("./wav/*"), key=os.path.getctime)     ## 분리된 wav 파일을 glob으로 읽음
-            print(self.FileList)
-            ETRI= ETRI_STT()					## ETRI stt를 활용하여 읽음 
-            print("Start...")
-            NewFileList = self.FileList				 
-            for fileListName in NewFileList:				
-                   self.checkCount = 0
-                   ip, text = ETRI.run(fileListName.split("\\")[-1])		## text  : stt 결과 spring형태의 text 결과가 저장된 변수값
-                   if ip in self.ipDict:				## ip(interpretation percent) : 요약과 keyword 추출시, 각 문장단위로 앞 문장과의 연관성과 전체 문장내에서의 해당 문장의 영향도를 분석하기 위한 변수 
-                       self.ipDict[ip] += text
-                   else:
-                       self.ipDict[ip] = text
-                     
-            time.sleep(5) 
-            self.checkCount += 1
-            text = " "
-            for key in self.ipDict.keys():				## kss NLP kaggle library 에서 제공하는 .key() 함수를 통해 문장을 단어단위로 분류함.  
-                text += self.ipDict[key]
-
-            #for text in self.ipDict[ip]:
-            #    for text_arr in text:
-            #        corpus += text_arr + " "
-            #sentense = sent_tokenize(corpus)
-            #text_sentense = ''.join(sentense)
-            keywords.extract(text)				## stt 결과값인 text 변수를 요약 알고리즘 (keywords.py) 함수로 실행 후, 요약된 문단과 keyword 단어 리턴 호출함.
-            return
+        folder = 'C:/Users/pmw18/Downloads/DGB_R&D_Project/dgb_reporter'
+        file = 'YTN_bong_3.wav'
+        split_wav = SplitWavAudioMubin(folder, file)                                 
+        split_wav.multiple_split(min_per_split=1)			## 전체 wav파일을 1분 이하의 파일들로 분리함 
+        self.FileList = sorted(glob("./wav/*"), key=os.path.getctime)     ## 분리된 wav 파일을 glob으로 읽음
+        print(self.FileList)
+        ETRI= ETRI_STT()					## ETRI stt를 활용하여 읽음 
+        print("Start...")
+        NewFileList = self.FileList				 
+        for fileListName in NewFileList:				
+            self.checkCount = 0
+            ip, text = ETRI.run(fileListName.split("\\")[-1])		## text  : stt 결과 spring형태의 text 결과가 저장된 변수값
+            if ip in self.ipDict:				## ip(interpretation percent) : 요약과 keyword 추출시, 각 문장단위로 앞 문장과의 연관성과 전체 문장내에서의 해당 문장의 영향도를 분석하기 위한 변수 
+                self.ipDict[ip] += text
+            else:
+                self.ipDict[ip] = text
                 
-        except KeyboardInterrupt as e: # 사용자에 의해 "ctrl + z" 발생시
-            print ("감시 중지...")
+        time.sleep(5) 
+        self.checkCount += 1
+        text = " "
+        for key in self.ipDict.keys():				## kss NLP kaggle library 에서 제공하는 .key() 함수를 통해 문장을 단어단위로 분류함.  
+            text += self.ipDict[key]
+
+        #for text in self.ipDict[ip]:
+        #    for text_arr in text:
+        #        corpus += text_arr + " "
+        #sentense = sent_tokenize(corpus)
+        #text_sentense = ''.join(sentense)
+        keywords.extract(text)				## stt 결과값인 text 변수를 요약 알고리즘 (keywords.py) 함수로 실행 후, 요약된 문단과 keyword 단어 리턴 호출함.
+        return
 
 if __name__ == "__main__":
     WATCHDOG = watchdog()
